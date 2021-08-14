e416bfbd483d1866819d37aafcdb21ed940cb34c
Ashwani <ashwani.wuid@gmail.com>
2021-08-14 23:22:30 +0530

    added postgres

--- byoh/settings.py
+++ byoh/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'byoh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'byoh',
+        'USER': 'postgres',
+        'PASSWORD': 'abcdef',
+        'HOST': 'localhost'
+
+
     }
 }
 
