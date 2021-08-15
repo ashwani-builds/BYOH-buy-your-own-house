f6dc367cae02a98ddd21498d1fe144b15249d18a
Ashwani <ashwani.wuid@gmail.com>
2021-08-15 18:32:30 +0530

    made the listing page dynamic

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from listings.models import Listing
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context=context)
 
 
 def listing(request):
